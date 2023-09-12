4728fe19b7ed6a5d17b08a4f32d16a6a978cc743
Shaurya Pratap Singh Tomar <38722514+Shauryapst@users.noreply.github.com>
2023-09-12 23:07:43 +0530

    test

--- src/components/Card/CardSmall/CardSmall.js
+++ src/components/Card/CardSmall/CardSmall.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React from "react";
 import "./CardSmall.css";
 
-function CardSmall() {
+const CardSmall = (props) => {
+  
   return (
+    
     <div class="product-card">
-        <img src="https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg" alt="" class="product-image"/>
-        <div class="product-details">
-            <h3 class="product-name">Product Name</h3>
-            <p class="product-price">$19.99</p>
-        </div>
+      <img
+        src={props.data.image}
+        alt=""
+        class="product-image"
+      />
+      <div class="product-details">
+        <h3 class="product-name">{props.data.title}</h3>
+        <p class="product-price">${props.data.price}</p>
+      </div>
     </div>
-  )
+  );
 }
 
 export default CardSmall;
--- src/features/Men/Men.js
+++ src/features/Men/Men.js
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CardSmall from '../../components/Card/CardSmall/CardSmall';
+import apiHelper from '../../helper/api.helper';
+import './Men.css'
 
 const Men = () => {
-  return (
-    <CardSmall/>
-  )
-}
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-export default Men
+  const getMenData = async () => {
+    try {
+      const response = await apiHelper.sendFake({ url: `/products/category/jewelery`, method: 'get' });
+      return response;
+    } catch (err) {
+      setError(err);
+      throw err;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    setLoading(true);
+    getMenData()
+      .then((responseData) => {
+        setData(responseData);
+      })
+      .catch((error) => {
+        console.error('Error fetching men data:', error);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div className='product-page'>
+      <div className='product-section'>
+        {data.map((item) => (
+          <CardSmall key={item.id} data={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Men;
