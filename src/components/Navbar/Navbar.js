9fa2eca162bd6eda7b09823e32eb27d12148283a
shaurya <shauryapst1@gmail.com>
2024-02-18 22:53:26 +0530

    Added cart in navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,28 +1,43 @@
-import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link from React Router
-import './Navbar.css';
+import React, { useState } from "react";
+import { Link } from "react-router-dom"; // Import Link from React Router
+import "./Navbar.css";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleCart } from "../../slices/cartOverlaySlice";
 
 const Navbar = () => {
+  const dispatch = useDispatch();
+  const cartItemCount = useSelector((state) => state.cart.count);
   return (
     <nav className="navbar">
       <div className="navbar-content">
         <h1 className="website-name">FAKE STORE</h1>
-        <ul className="menu">
-          <li className="menu-item">
-            <Link to="/">Home</Link>
-          </li>
-          
-          <li className="menu-item">
-            <Link to="/category/men">Men</Link>
-          </li>
-          <li className="menu-item">
-            <Link to="/category/women">Women</Link>
-          </li>
-          <li className="menu-item">
-            <Link to="/category/jewelery">Jewelery</Link>
-          </li>
-          
-        </ul>
+        <div className="">
+          <ul className="menu">
+            <li className="menu-item">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="menu-item">
+              <Link to="/category/men">Men</Link>
+            </li>
+            <li className="menu-item">
+              <Link to="/category/women">Women</Link>
+            </li>
+            <li className="menu-item">
+              <Link to="/category/jewelery">Jewelery</Link>
+            </li>
+            <div
+              className="menu-item"
+              onClick={() => {
+                dispatch(toggleCart());
+              }}
+            >
+              <a class="cart-icon-container">
+                Cart
+                <span class="cart-count">{cartItemCount}</span>
+              </a>
+            </div>
+          </ul>
+        </div>
       </div>
     </nav>
   );
