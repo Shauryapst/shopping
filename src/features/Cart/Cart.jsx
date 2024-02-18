504c73a7cc0aa3f667e26d3ca6b7b4aaba59fa53
shaurya <shauryapst1@gmail.com>
2024-02-18 22:53:56 +0530

    cart floating modal

--- src/features/Cart/Cart.jsx
+++ src/features/Cart/Cart.jsx
@@ -1,26 +1,42 @@
 import React from "react";
 import "./Cart.css";
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch } from "react-redux";
 import { toggleCart } from "../../slices/cartOverlaySlice";
-
+import CartItem from "../../components/CartItem/CartItem";
 
 const Cart = () => {
-  const isOpen = useSelector((state) => state.cartToggle.value)
-  const cartList = useSelector((state) => state.cart);
-  console.log(cartList);
-  const dispatch = useDispatch()
+  const isOpen = useSelector((state) => state.cartToggle.value);
+  const cartData = useSelector((state) => state.cart);
+  const cartItems = cartData.cartItems;
+
+  const totalPrice = Object.values(cartItems).reduce(
+    (total, item) => total + item.price * item.count,
+    0
+  );
+
+  const dispatch = useDispatch();
+
   return (
     <div className={`cart-overlay ${isOpen ? "open" : ""}`}>
-     
       <div className="cart-content">
         <h2>Cart Details</h2>
-        <p>Item 1</p>
-        <p>Item 2</p>
-        <p>Item 3</p>
-         <button onClick={()=> dispatch(toggleCart())}>X</button>
+        <button className="cancel-btn" onClick={() => dispatch(toggleCart())}>
+          X
+        </button>
+        <div className="cart-items">
+          {Object.keys(cartItems).map((id) => (
+            <CartItem
+              key={id}
+              imageUrl={cartItems[id].image}
+              quantity={cartItems[id].count}
+              price={cartItems[id].price}
+              title={cartItems[id].title}
+            />
+          ))}
+        </div>
+        <div className="total-price">Total Price: ${totalPrice.toFixed(2)}</div>
+        <button className="checkout-btn">Checkout</button>
       </div>
-     
-      
     </div>
   );
 };
